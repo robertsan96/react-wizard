19af6899e77c42f19c5b530d98e7032941ffd9cd
Robert San <robertsandru96@gmail.com>
2020-04-25 00:08:51 +0300

    Mixins, variables, scss, new App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import * as styles from "./scss/App.module.scss";
+
 const App = () => {
-  return <p></p>;
+  return (
+    <div className={styles.AppWrapper}>
+      <div className={styles.Content}></div>
+    </div>
+  );
 };
 
 export default App;
